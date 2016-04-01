3bbdbd7b2cd97db21c063bfe6d78e35ceb83f3a0
Samiha Rahman <Samiha Rahman>
2016-03-31 23:50:12 -0700

    Cleaning more files

--- .metadata/P4_Akilesh_Samiha_DungeonCrawler/src/DungeonCrawler_DCGame.java
+++
@@ -1,7 +0,0 @@
-
-public class DungeonCrawler_DCGame {
-	public static void main(String[] args) {
-		DungeonCrawler_Controller controller = new DungeonCrawler_Controller();
-		controller.play();
-	}
-}
